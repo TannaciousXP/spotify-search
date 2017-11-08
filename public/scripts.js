a3418928beebb94f62f81191377c68cbaa20de72
Peter Tan <pxhtan@gmail.com>
2017-11-08 10:45:07 -0800

    add comments

--- public/scripts.js
+++ public/scripts.js
@@ -1,14 +1,116 @@
 // make document shorter
 let d = document;
 
+// create an element and set attributes;
+let createEleWithAttr = (ele, attr) => {
+  attr = attr || {};
+  let element = d.createElement(ele);
+  for (let k in attr) {
+    element.setAttribute(k, attr[k]);
+  }
+  return element;
+};
+// create Search for Information
+let createUlForArtist = (artistInfo) => {
+  // create ul element for artist
+  let ulForArtist = createEleWithAttr('ul', {class: 'response'});
+  // have the artist innerHTML to their name
+  ulForArtist.innerHTML = `Search for Artist: ${artistInfo.name}`;
+  // create li element for id and set the innerHTML
+  let liArtistId = createEleWithAttr('li');
+  liArtistId.innerHTML = `Artist id: ${artistInfo.id}`;
+  // create li for genres and set the innerHTML
+  let liGenres = createEleWithAttr('li');
+  liGenres.innerHTML = `Genres Array: ${artistInfo.genres}`;
+  // make and array and append li elements to artist
+  [liArtistId, liGenres].forEach((li => ulForArtist.appendChild(li)));
+  // make a gap
+  ulForArtist.appendChild(createEleWithAttr('br'));
+  // return the artist ul element
+  return ulForArtist;
+};
+// Related artist info
+let createUlForRelatedArtist = (relatedArtist) => {
+  // create Ul for related artist
+  let artist = createEleWithAttr('ul', {class: 'response'});
+  artist.innerHTML = `Related artist: ${relatedArtist[0].name}`;
+  // create li element for popularity
+  let popularity = createEleWithAttr('li');
+  popularity.innerHTML = `Popularity: ${relatedArtist[0].popularity}`;
+  // create li element for genres
+  let genres = createEleWithAttr('li');
+  genres.innerHTML = `Genres: ${relatedArtist[0].genres}`;
+  // create li element for matching tags
+  let matching = createEleWithAttr('li');
+  matching.innerHTML = `Matching genre tags: ${relatedArtist[1]}`;
+  // append the li elements to ui artist
+  [matching, popularity, genres].forEach(li => artist.appendChild(li));
+  // return artist
+  return artist;
+
+};
+// append name of songs to artist
+let appendSongsToArtist = (songsList, artist) => {
+  // create li variable
+  let li;
+  // start count at 1
+  let count = 1;
+  // use for of loop
+  for (let song of songsList) {
+    // each song, create an li element for the name of the song
+    li = createEleWithAttr('li');
+    li.innerHTML = `${count}: ${song}`;
+    // apppend li to artist
+    artist.appendChild(li);
+    // increat count
+    count++;
+  }
+};
+// create Ul List for each artist
+let renderArtistList = (artistList, songsList, appendTo) => {
+  // create artistUl variable
+  let artistUl;
+  // for in loop over artistList, songsList should match accordingly
+  for (let i = 0; i < artistList.length; i++) {
+    // create Ul for artist
+    artistUl = createUlForRelatedArtist(artistList[i]);
+    // append the songs to the artist
+    appendSongsToArtist(songsList[i], artistUl);
+    // append to response
+    appendTo.appendChild(artistUl);
+    // add space
+    appendTo.appendChild(createEleWithAttr('br'));
+  }
+};
+
+
 // make call to server to get list of information
 let getLists = () => {
-  let value = d.getElementById('display').value;
+  // get the value of the input form
+  let value = d.getElementById('display');
+  // get wrapper
+  let wrapper = d.getElementById('wrapper');
+  // get the response or create a new one if response === null
+  let response = d.getElementById('response') !== null ?
+                          d.getElementById('response') :
+                          createEleWithAttr('div', {id: 'response'});
+  response.innerHTML = 'Please wait while the magic happens in the background';
 
-  let req = new Request(`http://localhost:3000/getList?q=${value}`, {
+  wrapper.appendChild(response);
+  // let we = createEleWithAttr('div');
+  // console.log(we);
+  let req = new Request(`http://localhost:3000/getList?q=${value.value}`, {
     method: 'POST'
   });
+
   fetch(req).then(res => {
     return res.json();
-  }).then(data => console.log(data)).catch(err => console.warn('Request failed', err));
+  }).then(data => {
+    value.value = '';
+    response.innerHTML = null;
+    let searchFor = createUlForArtist(data[0]);
+    response.appendChild(searchFor);
+
+    renderArtistList(data[1], data[2], response);
+  }).catch(err => console.warn('Request failed', err));
 };
--- server/helpers/helperFuncs.js
+++ server/helpers/helperFuncs.js
@@ -2,6 +2,8 @@ module.exports = {
   rp: require('request-promise'),
   // find out how many matching genre tags there are
   idxToInsert: function(artist, genresArr) {
+    // track how many tags are matching, and we want to stay away from repeat matching
+    // so I use splice
     let idx = 0;
     for (let genre of artist.genres) {
       let i = genresArr.indexOf(genre);
@@ -55,21 +57,29 @@ module.exports = {
   },
   // return the list of artists sorted by popularity and how many tags match
   sortBySimThenPop: function(artistArr, genresArr) {
+    // create an array of matching tags array
     let output = Array.apply(null, Array(genresArr.length));
     output = output.map(ele => []);
+    // loop through the artist to see how many tags match
     for (let artist of artistArr) {
+      // find out how many tags match if there is no matching tags we don't use the artist
       let i = this.idxToInsert(artist, genresArr.slice());
+      // use insert to sort them into matching tags arr and sort by popularity
       if (i > 0) {
         this.insert(output[genresArr.length - i], artist);
       }
     }
+    // after everything is completed, we sort the array into one level [artist, macthingTags]
+    // for each artist
     output = this.resortNestedArr(output, genresArr.length);
     return output;
   },
   // get an array of artist albums;
   getAlbums: function(sortedArr, token) {
+    // create an array to use promise all
     let output = [];
     let albumsOpt;
+    // run a for loop to push api call into output
     for (let i = 0; i < sortedArr.length; i++) {
       albumsOpt = {
         url: `https://api.spotify.com/v1/artists/${sortedArr[i][0].id}/albums?album_type=album&market=US`,
@@ -80,6 +90,7 @@ module.exports = {
       };
       output.push(this.rp.get(albumsOpt));
     }
+    // create a new promise to resolve all api calls
     return new Promise(function(resolve, reject) {
       Promise.all(output).then(function(values) {
         resolve(values);
@@ -96,6 +107,7 @@ module.exports = {
     let rand, songs, songsRatio, songsDebt, songsIdx, songsLength;
     // actions for one only album
     if (artistAlbums[idx].albums.length === 1) {
+      // check to see if all the array had 15 songs
       let onlyAlbum = artistAlbums[idx].albums[0].tracks.items;
       if (onlyAlbum.length < 15) {
         for (let track of onlyAlbum) {
@@ -119,13 +131,6 @@ module.exports = {
         return count;
       }, 0)) / 15;
 
-
-      // console.log(ratio);
-      // console.log('then');
-      // for (let v of albumsNPopularity) {
-      //   console.log(v[1]);
-      // }
-      // console.log('end');
 
       for (let albNPop of albumsNPopularity) {
         songs = albNPop[0].tracks.items;
@@ -160,24 +165,38 @@ module.exports = {
           }
         }
       }
-
+      // check if the songs is equal to 15
       if (songsList.length !== 15) {
+        // condition for if songsList is > 15
         if (songsList.length > 15) {
+          // find out how many is over
           let toPop = songsList.length - 15;
+          // use a forloop to pop them out
           for (let i = 0; i < toPop; i++) {
             songsList.pop();
           }
+          // check to see if the songs is < 15
         } else if (songsList.length < 15) {
-          let toAdd = 15 - songsList.length;
-          while (toAdd !== 0) {
-            rand = albumsNPopularity[Math.random() * albumsNPopularity.length >> 0];
-            songs = rand[0].tracks.items;
-            if (songs.length !== 0) {
-              songsIdx = Math.random() * songs.length >> 0;
-              rand = songs[songsIdx];
-              songsList.push(rand.name);
-              songs.splice(idx, 1);
-              toAdd--;
+          // check condition to see if there is only 1 album and no more songs
+          if (artistAlbums[idx].albums.length === 1 && artistAlbums[idx].albums[0].tracks.items.length === 0) {
+            // break out of if
+            // do nothing if there is not enough songs;
+          } else {
+            // find out to add
+            let toAdd = 15 - songsList.length;
+            // use a while loop to fill in missing songs
+            while (toAdd !== 0) {
+              // get a random album
+              rand = albumsNPopularity[Math.random() * albumsNPopularity.length >> 0];
+              // get the random tracks
+              songs = rand[0].tracks.items;
+              if (songs.length !== 0) {
+                songsIdx = Math.random() * songs.length >> 0;
+                rand = songs[songsIdx];
+                songsList.push(rand.name);
+                songs.splice(idx, 1);
+                toAdd--;
+              }
             }
           }
         }
@@ -196,9 +215,12 @@ module.exports = {
     }
     return albNTracks;
   },
+  // get albums tracks api using promise all
   getAlbumsNTracks: function(albumsList, token) {
+    // set up array for output
     let output = [];
     let albumsNTracksOpt, albumsStr;
+    // push all api calls into output
     for (let albums of albumsList) {
       albumsStr = '';
       for (let i = 0; i < albums.items.length; i++) {
@@ -218,7 +240,7 @@ module.exports = {
       output.push(this.rp.get(albumsNTracksOpt));
     }
 
-
+    // return promise api calls
     return new Promise (function(resolve, reject) {
       Promise.all(output).then(albNTracks => {
         // call the the func getAlbums and Track
